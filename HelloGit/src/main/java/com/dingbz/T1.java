fa607a8750e69e1d3ef94ec6de59d0639f08c03a
onlysingleone <382522671@qq.com>
2021-08-22 18:55:07 +0800

    commit

---
+++ HelloGit/src/main/java/com/dingbz/T1.java
@@ -0,0 +1,12 @@
+package com.dingbz;
+
+/**
+ * @author Ding_Baozhong
+ * @create 2021-08-22 18:52
+ */
+public class T1 {
+
+    public static void main(String[] args) {
+        System.out.println ("112");
+    }
+}
---
+++ HelloGit/src/main/java/com/dingbz/T2.java
@@ -0,0 +1,12 @@
+package com.dingbz;
+
+/**
+ * @author Ding_Baozhong
+ * @create 2021-08-22 18:53
+ */
+public class T2 {
+
+    public static void main(String[] args) {
+        System.out.println ("112");
+    }
+}
